734919849ed130b69df2c1a2a0a1c4636bb2e0b7
sridamul <sridamul@gmail.com>
2025-04-17 01:59:14 +0530

    feat(component/validation): add validation of sortidx and compinline exports
    * Indices in sortidx are validated according to sort's index spaces
    * validate acutal type supplied is subsituted for all uses of the import
    
    Signed-off-by: sridamul <sridamul@gmail.com>

--- test/component/componentvalidatortest.cpp
+++ test/component/componentvalidatortest.cpp
@@ -1,5 +1,6 @@
 #include "common/errinfo.h"
 #include "validator/validator_component.h"
+#include "vm/vm.h"
 #include <gtest/gtest.h>
 
 namespace WasmEdge {
@@ -35,118 +36,16 @@ static std::shared_ptr<AST::Component::Component>
 createComponent(const AST::Component::ComponentSection &CompSec) {
   return std::make_shared<AST::Component::Component>(CompSec.getContent());
 }
-TEST(ComponentValidatorTest, CorrectMatchingMultipleComponents) {
-  AST::Component::DescTypeIndex Desc;
-  Desc.getIndex() = 0;
-  Desc.getKind() = AST::Component::IndexKind::FuncType;
 
-  AST::Component::Import ImportF;
-  ImportF.getName() = "f";
-  ImportF.getDesc() = Desc;
-
-  AST::Component::Import ImportG;
-  ImportG.getName() = "g";
-  ImportG.getDesc() = Desc;
-
-  auto ImportSec0 = createImportSection({ImportF});
-  auto ImportSec1 = createImportSection({ImportG});
-
-  AST::Component::ComponentSection CompSec0;
-  CompSec0.getContent() = std::make_shared<AST::Component::Component>();
-  CompSec0.getContent()->getSections().push_back(ImportSec0);
-
-  AST::Component::ComponentSection CompSec1;
-  CompSec1.getContent() = std::make_shared<AST::Component::Component>();
-  CompSec1.getContent()->getSections().push_back(ImportSec1);
-
-  AST::Component::SortIndex<AST::Component::Sort> SortIdx0;
-  SortIdx0.getSort() = AST::Component::SortCase::Func;
-  SortIdx0.getSortIdx() = 0;
-
-  AST::Component::InstantiateArg<
-      AST::Component::SortIndex<AST::Component::Sort>>
-      InstArg0;
-  InstArg0.getName() = "f";
-  InstArg0.getIndex() = SortIdx0;
-
-  AST::Component::Instantiate Inst0(0, {InstArg0});
-
-  AST::Component::SortIndex<AST::Component::Sort> SortIdx1;
-  SortIdx1.getSort() = AST::Component::SortCase::Func;
-  SortIdx1.getSortIdx() = 0;
-
-  AST::Component::InstantiateArg<
-      AST::Component::SortIndex<AST::Component::Sort>>
-      InstArg1;
-  InstArg1.getName() = "g";
-  InstArg1.getIndex() = SortIdx1;
-
-  AST::Component::Instantiate Inst1(1, {InstArg1});
-
-  AST::Component::InstanceSection InstSec;
-  InstSec.getContent().push_back(Inst0);
-  InstSec.getContent().push_back(Inst1);
-
-  AST::Component::ComponentSection OuterCompSec;
-  OuterCompSec.getContent() = std::make_shared<AST::Component::Component>();
-  OuterCompSec.getContent()->getSections().push_back(CompSec0);
-  OuterCompSec.getContent()->getSections().push_back(CompSec1);
-  OuterCompSec.getContent()->getSections().push_back(InstSec);
-
-  auto C0 = createComponent(OuterCompSec);
-
-  WasmEdge::Configure Conf;
-  WasmEdge::Validator::Validator Validator(Conf);
-
-  assertOk(Validator.validate(*C0),
-           "Validation should pass for multiple correctly matched components");
-}
-
-TEST(ComponentValidatorTest, NestedCorrectMatching) {
-  AST::Component::DescTypeIndex Desc;
-  Desc.getIndex() = 0;
-  Desc.getKind() = AST::Component::IndexKind::FuncType;
-
-  AST::Component::Import ImportF;
-  ImportF.getName() = "f";
-  ImportF.getDesc() = Desc;
-
-  auto ImportSec = createImportSection({ImportF});
-
-  AST::Component::ComponentSection CompSec;
-  CompSec.getContent() = std::make_shared<AST::Component::Component>();
-  CompSec.getContent()->getSections().push_back(ImportSec);
-
-  AST::Component::SortIndex<AST::Component::Sort> SortIdx;
-  SortIdx.getSort() = AST::Component::SortCase::Func;
-  SortIdx.getSortIdx() = 0;
-
-  AST::Component::InstantiateArg<
-      AST::Component::SortIndex<AST::Component::Sort>>
-      InstArg;
-  InstArg.getName() = "f";
-  InstArg.getIndex() = SortIdx;
-
-  std::vector<AST::Component::InstantiateArg<
-      AST::Component::SortIndex<AST::Component::Sort>>>
-      Args = {InstArg};
-  AST::Component::Instantiate Inst(0, std::move(Args));
-
-  AST::Component::InstanceSection InstSec;
-  InstSec.getContent().push_back(Inst);
-
-  AST::Component::ComponentSection OuterCompSec;
-  OuterCompSec.getContent() = std::make_shared<AST::Component::Component>();
-  OuterCompSec.getContent()->getSections().push_back(CompSec);
-  OuterCompSec.getContent()->getSections().push_back(InstSec);
-
-  auto C0 = createComponent(OuterCompSec);
-
-  WasmEdge::Configure Conf;
-  WasmEdge::Validator::Validator Validator(Conf);
-
-  assertOk(Validator.validate(*C0),
-           "Validation should pass for correct matching");
+TEST(Component, LoadAndRun_TestWasm) {
+  Configure Conf{};
+  Conf.addProposal(Proposal::Component);
+  VM::VM VM{Conf};
+  std::filesystem::path FilePath = std::filesystem::current_path() /
+                                   "test/component/componentTestData/test.wasm";
+  assertOk(VM.loadWasm(FilePath), "failed to load component binary");
+  assertOk(VM.loadWasm(FilePath), "failed to load component binary");
+  assertOk(VM.validate(), "failed to validate");
 }
 
 TEST(ComponentValidatorTest, MissingArgument) {
